e8fad433d84bf5bdb06cf38b7f35732d9697f960
Jose Alberto Alves <alveslemosneto@gmail.com>
2025-05-18 16:20:43 -0300

    upgrades

--- dailycoding/ex001.js
+++ dailycoding/ex001.js
@@ -3,8 +3,11 @@
 
 var name=window.prompt("What is your name?")
 
+
+
 document.write(`Seu nome tem ${name.length} letras <br/>`)
 document.write(`Seu nome em maiúsculo é ${name.toUpperCase()}`)
 
 
 n1.toLocalString('pt-BR',{style:'currency',currency:'BRL'})
+
