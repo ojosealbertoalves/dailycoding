ca24e67ad3eb69312a9d8956a5154121f402c09b
Jose Alberto Alves <alveslemosneto@gmail.com>
2025-05-20 10:01:15 -0300

    date and time

---
+++ exercises/time_day_parent/time-day/script.js
@@ -0,0 +1,15 @@
+function load(){
+  const msg=window.document.getElementById("msg")
+  const img=window.document.getElementById("img")
+  const date=new Date()
+  //const time=date.getHours()
+  const time=15
+  msg.innerHTML=`Now is ${time} hours`
+  if(time>=0 && time<12){
+    //Good morning
+    img.src="images/night.png"
+  }else if(time>=12 && time<18){
+     img.src="images/afternoon.png"
+
+  }
+}
