024288d9ed871b60ec9fd53f09e2706370437749
Jose Alberto Alves <alveslemosneto@gmail.com>
2025-06-09 19:48:53 -0300

    calculator rv

--- exercises/calculator/app.js
+++ exercises/calculator/app.js
@@ -5,10 +5,26 @@ let number1=0;
 
      function updateoperation(op){
         operation=op;
+        number1=parseInt(input.value);
+        input.value="0";
     }
 
+function clear(){
+    input.value="0";
+}
+
 function solve(){
-    alert(solve)
+    let number2=parseInt(input.value);
+    let number3=0;
+
+    switch(operation){
+        case '+': number3=number1+number2; break;
+        case '-': number3=number1-number2; break;
+        case 'x': number3=number1*number2; break;
+        case '/': number3=number1/number2; break; 
+    }
+  
+    input.value=number3;
   }
 
 function updatedisplay(number){
