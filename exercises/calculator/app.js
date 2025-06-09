b66cbeaecfbcd551973e3783a94fd9d8c66658ef
Jose Alberto Alves <alveslemosneto@gmail.com>
2025-06-09 19:29:39 -0300

    calculator-functions

---
+++ exercises/calculator/app.js
@@ -0,0 +1,21 @@
+const input=document.getElementById("display")
+let operation="";
+let number1=0;
+
+
+     function updateoperation(op){
+        operation=op;
+    }
+
+function solve(){
+    alert(solve)
+  }
+
+function updatedisplay(number){
+
+if(input.value==="0")
+    input.value=number
+else
+    input.value=input.value+number
+
+}
