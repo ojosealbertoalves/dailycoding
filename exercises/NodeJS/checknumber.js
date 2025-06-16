b6a1fe5aef1016181e01f212f4e874461111531d
Jose Alberto Alves <alveslemosneto@gmail.com>
2025-06-16 16:26:24 -0300

    checknumber

---
+++ exercises/NodeJS/checknumber.js
@@ -0,0 +1,9 @@
+function checknumber(number) {
+    if (number % 2 === 0) {
+        console.log("This is an even number");
+    } else {
+        console.log("This is an odd number");
+    }
+}
+
+module.exports={checknumber};
