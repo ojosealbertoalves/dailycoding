5bc2db03d846ec1fd8e165f9d7c15aec00f0ff70
Jose Alberto Alves <alveslemosneto@gmail.com>
2025-06-16 16:17:30 -0300

    NodeJS-calculator-exercise

---
+++ exercises/NodeJS/calculator.js
@@ -0,0 +1,18 @@
+function sum(n1,n2){
+return n1+n2
+}
+
+function subtract(n1,n2){
+return n1-n2
+}
+
+
+function multiply(n1,n2){
+return n1*n2
+}
+
+function division(n1,n2){
+return n1/n2
+}
+
+module.exports={sum, subtract,multiply,division};
